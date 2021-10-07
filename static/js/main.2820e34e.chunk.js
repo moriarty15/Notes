eeea989ae635c1b5848d8931935ed55d94e8f9ee
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2021-10-07 11:09:02 +0800

    Updates

---
+++ static/js/main.2820e34e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{2:function(p,s,n){"use strict";n.r(s);n(0),n(4),n(8),n(9);console.log("\u043f\u0440\u0438\u0432\u0435\u0442")},9:function(p,s){}},[[2,1,2]]]);
+//# sourceMappingURL=main.2820e34e.chunk.js.map
